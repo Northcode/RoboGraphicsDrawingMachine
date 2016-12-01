b00af1c96ab855fd4bc9e989b83cb42e42eb8be5
Andreas Larsen <northcode.no@gmail.com>
2016-12-01 02:04:10 +0100

    Added barebones RoboGraphics

---
+++ src/main/java/no.northcode.tdd/RoboGraphics.java
@@ -0,0 +1,13 @@
+package no.northcode.tdd;
+
+public class RoboGraphics {
+
+    public void penUp() {}
+    public void penDown() {}
+    public void turnRight() {}
+    public void turnLeft() {}
+
+
+    public void move(int steps) {}
+    
+}
